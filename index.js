055be4be5182eb8281213c74cf0b5ebeb6ec3769
jgbispo <jgbispo20@gmail.com>
2023-02-11 21:26:54 -0300

    Make integration to front and back end
    O back-end esta conectado ao fron-end, agora é fazer a comunicação co a
    IA

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
   res.status(200).send('/index.html');
 });
 
 app.post('/api', (req, res) => {
-  res.status(200).json({ message: 'Requisição recebida com sucesso!', body: req.body });
+  const { prompt } = req.body;
+
+  res.status(200).json({ message: 'Requisição recebida com sucesso!', body: prompt });
 });
 
 app.listen(PORT, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,24 @@
 const input = document.getElementById("input");
 const history = document.getElementById("history");
 
+const humanText = document.querySelectorAll(".human");
+const aiText = document.querySelectorAll(".ai");
+
+function makeQuestion() {
+  let question = "";
+  for (let i = 0; i < humanText.length; i++) {
+    question += `Human: ${humanText[i].innerText}\n`;
+    if (aiText[i]) question += `AI: ${aiText[i].innerText}\n`;
+  }
+  if (input.value) question += `Human: ${input.value}\n`;
+  return question;
+}
+
+
 // send data to server when press enter
 input.addEventListener('keyup', (e) => {
+  var sQuestion = makeQuestion();
+
   if (e.key === 'Enter') {
     const value = e.target.value;
     fetch('/api', {
@@ -10,11 +26,20 @@ input.addEventListener('keyup', (e) => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ value })
+      body: JSON.stringify({
+        "model": "text-davinci-003",
+        "prompt": sQuestion,
+        "temperature": 0.9,
+        "max_tokens": 150,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0.6,
+        "stop": [" Human:", " AI:"]
+      })
     }).then(res => res.json())
       .then(data => {
         console.log(data);
-        history.innerHTML += `<p class="human">${data.body.value}</p>`;
+        history.innerHTML += `<p class="human">${data.body}</p>`;
       })
   }
 })
